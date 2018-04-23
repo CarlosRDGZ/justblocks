3d5d9c56711a76252a524a61eabc0f8a14e2cf3d
Carlos Francisco Rodriguez Aguilar <crodriguez20@ucol.mx>
2018-04-23 17:30:40 -0500

    Solo funciones CRUD en api/announcement.js

--- api/announcement.js
+++ api/announcement.js
@@ -7,6 +7,78 @@ const announcementFindMiddleware = require("../middlewares/findAnnouncement");
 announcements.use(bodyParser.urlencoded({ extended: true }))
 announcements.use(bodyParser.json())
 
+announcements.use('/', sessionMiddleware);
+announcements.use('/:id*', sessionMiddleware);
+
+announcements.route('/')
+  .get((req,res) => { //Le regresa todas sus convocatorias (las que el dio de alta)
+    console.log("GET announcement");
+    Announcement.find({idCreator: res.locals.user._id}, function(err, announcementsGot) {
+      if(err){res.redirect("/app"); return;}
+      res.json(announcementsGot);//Todas las comvocatorias del usuario
+    })
+  })
+  .post((req,res) => {
+      console.log("POST announcement");
+      console.log(req.session.user_id);
+      var body = req.body;
+      var announcement = new Announcement({
+        idCreator: /*"5ad745e966e45e2f9c0df8ec"*/req.session.user_id,
+        creationDate: body.creationDate,
+        evaluationDate: body.evaluationDate,
+        deadlineDate: body.deadlineDate,
+        evaluatorsAmount: body.evaluatorsAmount,
+        projectsByEvaluator: body.projectsByEvaluator,
+        content: body.content
+      });
+
+      announcement.save().then(function(announcementsaved) {
+        //Lo logue si sí se pudo guardar el usuario
+        res.json(announcementsaved);
+      }).catch(function(err) {
+        console.log(err.message);
+          res.json({err: err.message/*"Hubo un problema al guardar el usuario"*/});
+      })
+  })
+
+announcements.route('/:id')
+  .put(function(req, res) {
+    var announcement = new Announcement({
+        _id: req.params.id,
+        idCreator: /*"5ad745e966e45e2f9c0df8ec"*/req.locals.user._id,
+        creationDate: body.creationDate,
+        evaluationDate: body.evaluationDate,
+        deadlineDate: body.deadlineDate,
+        evaluatorsAmount: body.evaluatorsAmount,
+        projectsByEvaluator: body.projectsByEvaluator,
+        content: body.content
+      });
+
+    announcement.save().then(function(announcementsaved) {
+        //Lo logue si sí se pudo guardar el usuario
+        res.json(announcementsaved);
+      }).catch(function(err) {
+        console.log(err.message);
+          res.json({err: err.message/*"Hubo un problema al guardar el usuario"*/});
+      })
+  })
+  .delete(function(req, res) {
+    console.log("DELETE announcement");
+    console.log(req.params.id);
+    Announcement.findOneAndRemove({_id: req.params.id}, function(err) {
+      if(!err) {
+        res.json(req.params.id);
+      }
+      else {
+        console.log(err);
+        res.json({err: err});
+      }
+    })
+  })
+
+  module.exports = announcements;
+
+/** NO CRUD
 announcements.get("/", function(req, res) {
   res.render("announcements");
 })
@@ -39,74 +111,4 @@ announcements.get("/view/:id", function(req, res) {
     }
   })
 })
-
-announcements.use("/announcement", sessionMiddleware);
-announcements.use("/announcement/:id*", sessionMiddleware);
-
-announcements.route('/announcement')
-  .get((req,res) => { //Le regresa todas sus convocatorias (las que el dio de alta)
-    console.log("GET announcement");
-    Announcement.find({idCreator: res.locals.user._id}, function(err, announcementsGot) {
-      if(err){res.redirect("/app"); return;}
-      res.json(announcementsGot);//Todas las comvocatorias del usuario
-    })
-  })
-  .post((req,res) => {
-      console.log("POST announcement");
-      console.log(req.session.user_id);
-      var body = req.body;
-      var announcement = new Announcement({
-        idCreator: /*"5ad745e966e45e2f9c0df8ec"*/req.session.user_id,
-        creationDate: body.creationDate,
-        evaluationDate: body.evaluationDate,
-        deadlineDate: body.deadlineDate,
-        evaluatorsAmount: body.evaluatorsAmount,
-        projectsByEvaluator: body.projectsByEvaluator,
-        content: body.content
-      });
-
-      announcement.save().then(function(announcementsaved) {
-        //Lo logue si sí se pudo guardar el usuario
-        res.json(announcementsaved);
-      }).catch(function(err) {
-        console.log(err.message);
-          res.json({err: err.message/*"Hubo un problema al guardar el usuario"*/});
-      })
-  })
-
-announcements.route('/announcement/:id')
-  .put(function(req, res) {
-    var announcement = new Announcement({
-        _id: req.params.id,
-        idCreator: /*"5ad745e966e45e2f9c0df8ec"*/req.locals.user._id,
-        creationDate: body.creationDate,
-        evaluationDate: body.evaluationDate,
-        deadlineDate: body.deadlineDate,
-        evaluatorsAmount: body.evaluatorsAmount,
-        projectsByEvaluator: body.projectsByEvaluator,
-        content: body.content
-      });
-
-    announcement.save().then(function(announcementsaved) {
-        //Lo logue si sí se pudo guardar el usuario
-        res.json(announcementsaved);
-      }).catch(function(err) {
-        console.log(err.message);
-          res.json({err: err.message/*"Hubo un problema al guardar el usuario"*/});
-      })
-  })
-  .delete(function(req, res) {
-    console.log("DELETE announcement");
-    console.log(req.params.id);
-    Announcement.findOneAndRemove({_id: req.params.id}, function(err) {
-      if(!err) {
-        res.json("/announcement/all");
-      }
-      else {
-        console.log(err);
-        res.json({err: err});
-      }
-    })
-  })
-
-module.exports = announcements;
+*/
