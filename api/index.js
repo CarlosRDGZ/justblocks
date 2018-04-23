e63847ff36bd2fcee0596b896a80f937be47bb65
Carlos Francisco Rodriguez Aguilar <crodriguez20@ucol.mx>
2018-04-23 17:23:36 -0500

    Deje solo los API CRUD en /api/index.js

--- api/index.js
+++ api/index.js
@@ -9,14 +9,4 @@ api.use('/user', users)
 api.use('/session', session)
 api.use('/announcement', announcement)
 
-api.get('/convocatoria', (req, res) => {
-  res.status(200).sendFile('convocatoria.html', { root: './public' })
-})
-
-api.get('/faq', (req,res) => {
-  res.status(200).render('faq.pug')
-})
-
-api.get('/', (req,res) => res.status(200).render('index.pug'))
-
 module.exports = api
