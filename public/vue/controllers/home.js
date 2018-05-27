f29b090b3ad9fbfdc75207931a301ef0c6bcb21f
CarlosRDGZ <crodriguez20@ucol.mx>
2018-05-26 23:36:55 -0500

    projects per user

--- public/vue/controllers/home.js
+++ public/vue/controllers/home.js
@@ -1,53 +1,127 @@
+let getDate = date => date != undefined ? date.substring(0,10) : 'NA'
+
+let Table = () => {
+	return {
+			pagination: {
+			current: 0,
+			pags: 0,
+			itemsPerPage: 10,
+			shown: {
+				first: -1,
+				last: -1,
+				pages: []
+			}
+		},
+		data: [],
+		index: {
+			top: -1,
+			bottom: -1
+		}
+	}
+}
+
+let pagination = (table,data) => {
+	return {
+		previous: function() {
+			if (table.pagination.current > 1) {
+				table.pagination.current--
+				table.data = 
+					data.slice(table.index.top -= table.pagination.itemsPerPage, table.index.bottom -= table.pagination.itemsPerPage)
+				if (table.pagination.current < table.pagination.shown.first){
+					table.pagination.shown.pages.unshift(table.pagination.current)
+					table.pagination.shown.pages = table.pagination.shown.pages.slice(0,table.pagination.shown.pages.length-1)
+					table.pagination.shown.first--
+					table.pagination.shown.last--
+				}
+			}
+		},
+		next: function() {
+			if (table.pagination.current < table.pagination.pags) {
+				table.pagination.current++
+				table.data = 
+					data.slice(table.index.top += table.pagination.itemsPerPage, table.index.bottom += table.pagination.itemsPerPage)
+				if (table.pagination.current > table.pagination.shown.last){
+					table.pagination.shown.pages.push(table.pagination.current)
+					table.pagination.shown.pages = table.pagination.shown.pages.slice(1)
+					table.pagination.shown.first++
+					table.pagination.shown.last++
+				}
+			}
+		},
+		goto: function(page) {
+			table.pagination.current = page
+			table.index.top = (table.pagination.itemsPerPage * page) - table.pagination.itemsPerPage
+			table.index.bottom = table.pagination.itemsPerPage * page
+			table.data =
+				data.slice(table.index.top, table.index.bottom)
+		},
+		first: function() {
+			table.pagination.shown.first = 1
+			table.pagination.shown.last = 5
+			table.pagination.shown.pages = []
+			for (let i = table.pagination.shown.first; i <= table.pagination.shown.last; i++)
+				table.pagination.shown.pages.push(i)
+				this.goto(1)
+		},
+		last: function() {
+			table.pagination.shown.first = table.pagination.pags - 4
+			table.pagination.shown.last = table.pagination.pags
+			table.pagination.shown.pages = []
+			for (let i = table.pagination.shown.first; i <= table.pagination.shown.last; i++)
+				table.pagination.shown.pages.push(i)
+			this.goto(table.pagination.pags)
+		}
+	}
+}
+
+let init = (table,data) => {
+	table.data = data
+	table.pagination.pags = Math.ceil(data.length / table.pagination.itemsPerPage)
+	table.pagination.current = data.length > 0 ? 1 : 0
+	table.data = data.slice(table.index.top = 0, table.index.bottom = table.pagination.itemsPerPage)
+	table.pagination.shown.first = table.pagination.current
+	table.pagination.shown.last = Math.min(table.pagination.pags,5)
+	for(let i = table.pagination.shown.first; i <= table.pagination.shown.last; i++)
+		table.pagination.shown.pages.push(i)
+}
+
 var vm = new Vue({
     el: '#app',
     data: {
 			announs: [],
+			projects: [],
 			tabs: {
 				announs: true,
-				evaluation: false,
+				projects: false,
 				selected: 'announs'
 			},
-			tableAnnouns: {
-				pagination: {
-					current: 0,
-					pags: 0,
-					itemsPerPage: 9,
-					shown: {
-						first: -1,
-						last: -1,
-						pages: []
-					}
-				},
-				data: [],
-				index: {
-					top: -1,
-					bottom: -1
-				}
+			table: {
+				announs: {},
+				projects: {}
 			}
 		},
 		created: function () {
+			this.table.announs = Table()
+			this.table.projects = Table()
 			window.axios.get(`/api/announcement/user/${id}`)
 				.then(({data}) => {
-					//Si no establecemos esta propiedad así no lo actualiza cuando carga la página
-					for(var i = 0; i < data.length; i++) {
-						data[i].partakers = 0;
-					}
-					data.forEach((announ) => {
-						getProjectsByAnnoun(announ._id, data);
-						announ.endEnrollmentsDate = announ.endEnrollmentsDate != undefined ? announ.endEnrollmentsDate.substring(0, 10) : 'NA';
-						announ.evaluationDate = announ.evaluationDate != undefined ? announ.evaluationDate.substring(0, 10) : 'NA';
-						announ.deadlineDate = announ.deadlineDate != undefined ? announ.deadlineDate.substring(0, 10) : 'NA';
+					data.forEach(e => {
+						e.partakers = 0
+						getProjectsByAnnoun(e._id, data);
+						e.endEnrollmentsDate = getDate(e.endEnrollmentsDate)
+						e.evaluationDate = getDate(e.evaluationDate)
+						e.deadlineDate = getDate(e.deadlineDate)
 					})
 					this.announs = data;
-					this.tableAnnouns.data = data
-					this.tableAnnouns.pagination.pags = Math.ceil(this.announs.length / this.tableAnnouns.pagination.itemsPerPage)
-					this.tableAnnouns.pagination.current = this.announs.length > 0 ? 1 : 0
-					this.tableAnnouns.data = this.announs.slice(this.tableAnnouns.index.top = 0, this.tableAnnouns.index.bottom = this.tableAnnouns.pagination.itemsPerPage)
-					this.tableAnnouns.pagination.shown.first = this.tableAnnouns.pagination.current
-					this.tableAnnouns.pagination.shown.last = Math.min(this.tableAnnouns.pagination.pags,10)
-					for(let i = this.tableAnnouns.pagination.shown.first; i <= this.tableAnnouns.pagination.shown.last; i++)
-						this.tableAnnouns.pagination.shown.pages.push(i)
+					init(this.table.announs, data)
 				})
+				window.axios.get(`/api/project/user/${id}`)
+				.then(({ data }) => {
+					console.log(0/0)
+					this.projects = data
+					init(this.table.projects, data)
+				})
+				
 		},
 		methods: {
 			changeContent: function (page) {
@@ -57,65 +131,15 @@ var vm = new Vue({
 					this.tabs.selected = page
 				}
 			},
-			table: function (self = this) {
+			table_: function (table,data) {
 				return {
-					pagination: function () {
-						return {
-							previous: function() {
-								if (self.tableAnnouns.pagination.current > 1) {
-									self.tableAnnouns.pagination.current--
-									self.tableAnnouns.data = 
-										self.announs.slice(self.tableAnnouns.index.top -= self.tableAnnouns.pagination.itemsPerPage, self.tableAnnouns.index.bottom -= self.tableAnnouns.pagination.itemsPerPage)
-										if (self.tableAnnouns.pagination.current < self.tableAnnouns.pagination.shown.first){
-											self.tableAnnouns.pagination.shown.pages.unshift(self.tableAnnouns.pagination.current)
-											self.tableAnnouns.pagination.shown.pages = self.tableAnnouns.pagination.shown.pages.slice(0,self.tableAnnouns.pagination.shown.pages.length-1)
-											self.tableAnnouns.pagination.shown.first--
-											self.tableAnnouns.pagination.shown.last--
-										}
-								}
-							},
-							next: function() {
-								if (self.tableAnnouns.pagination.current < self.tableAnnouns.pagination.pags) {
-									self.tableAnnouns.pagination.current++
-									self.tableAnnouns.data = 
-										self.announs.slice(self.tableAnnouns.index.top += self.tableAnnouns.pagination.itemsPerPage, self.tableAnnouns.index.bottom += self.tableAnnouns.pagination.itemsPerPage)
-									if (self.tableAnnouns.pagination.current > self.tableAnnouns.pagination.shown.last){
-										self.tableAnnouns.pagination.shown.pages.push(self.tableAnnouns.pagination.current)
-										self.tableAnnouns.pagination.shown.pages = self.tableAnnouns.pagination.shown.pages.slice(1)
-										self.tableAnnouns.pagination.shown.first++
-										self.tableAnnouns.pagination.shown.last++
-									}
-								}
-							},
-							goto: function(page) {
-								self.tableAnnouns.pagination.current = page
-								self.tableAnnouns.index.top = (self.tableAnnouns.pagination.itemsPerPage * page) - self.tableAnnouns.pagination.itemsPerPage
-								self.tableAnnouns.index.bottom = self.tableAnnouns.pagination.itemsPerPage * page
-								self.tableAnnouns.data =
-									self.announs.slice(self.tableAnnouns.index.top, self.tableAnnouns.index.bottom)
-							},
-							first: function() {
-								self.tableAnnouns.pagination.shown.first = 1
-								self.tableAnnouns.pagination.shown.last = 10
-								self.tableAnnouns.pagination.shown.pages = []
-								for (let i = self.tableAnnouns.pagination.shown.first; i <= self.tableAnnouns.pagination.shown.last; i++)
-									self.tableAnnouns.pagination.shown.pages.push(i)
-									this.goto(1)
-							},
-							last: function() {
-								self.tableAnnouns.pagination.shown.first = self.tableAnnouns.pagination.pags - 9
-								self.tableAnnouns.pagination.shown.last = self.tableAnnouns.pagination.pags
-								self.tableAnnouns.pagination.shown.pages = []
-								for (let i = self.tableAnnouns.pagination.shown.first; i <= self.tableAnnouns.pagination.shown.last; i++)
-									self.tableAnnouns.pagination.shown.pages.push(i)
-								this.goto(self.tableAnnouns.pagination.pags)
-							}
-						}
-					}
+					announs: pagination(table,data),
+					projects: pagination(table,data)
 				}
 			}
 		}
 })
+
 
 function getUserName(id, object) {
 	window.axios.get('/api/user/' + id + '/name')
