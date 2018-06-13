a123496c77fc52d1a3b89cec90d040944e1bf75e
Brandon Alejandro Mosqueda González <brandonMG_1698@hotmail.com>
2018-06-12 23:35:36 -0500

    Vista para calcular los promedios normales y la opción de los promedios ajustados una vez que todos los proyectos fueron calculados

--- app.js
+++ app.js
@@ -109,10 +109,10 @@ let t = 10, b = 5, k = 3
 const Project = require('./models/Project').Project;
 const Evaluator = require('./models/Evaluator').Evaluator
 //const Evaluator = require('./models/evaluator').Evaluator;
-let falseNames = [{"first": "Lana", "last": "Julien", }, {"first": "Alexandre", "last": "Brunet", }, {"first": "Elsa", "last": "Poirier", }, {"first": "Marc", "last": "Lamy", }, {"first": "Mila", "last": "Simon", }, {"first": "Florentin", "last": "Robert", }, {"first": "Adrian", "last": "Baron", }, {"first": "Anna", "last": "Menard", }, {"first": "Dylan", "last": "Roy", }, {"first": "Robin", "last": "Dupuy", }, {"first": "Colin", "last": "Bailly", }, {"first": "Alexandre", "last": "Antoine", }, {"first": "Pauline", "last": "Gaillard", }, {"first": "Clément", "last": "Andre", }, {"first": "Mateo Le", "last": "gall", }, {"first": "Lola", "last": "Germain", }, {"first": "Clara", "last": "Hamon", }, {"first": "Florian", "last": "Bertin", }, {"first": "Jasmine", "last": "Baron", }, {"first": "Martin", "last": "Bertrand", }, {"first": "Manon", "last": "Renaud", }, {"first": "Jeanne", "last": "Boyer", }, {"first": "Florentin", "last": "Lamy", }, {"first": "Amandine", "last": "Jacquet", }, {"first": "Yüna", "last": "Monnier", }, {"first": "Arthur", "last": "Gilbert", }, {"first": "Lamia", "last": "Besson", }, {"first": "Ambre", "last": "Roussel", }, {"first": "Antoine", "last": "Antoine", }, {"first": "Julie", "last": "Giraud", }, {"first": "Marion", "last": "Philippe", }, {"first": "Loevan", "last": "Gilbert", }, {"first": "Florian", "last": "Mercier", }, {"first": "Mathilde", "last": "Garcia", }, {"first": "Françoise", "last": "Denis", }, {"first": "Zacharis", "last": "Besson", }, {"first": "Ethan", "last": "Garcia", }, {"first": "Yohan", "last": "Fleury", }, {"first": "Candice", "last": "Denis", }, {"first": "Loevan Le", "last": "roux", }, {"first": "Adam", "last": "Rousseau", }, {"first": "Lamia Le", "last": "gall", }, {"first": "Lilian", "last": "Etienne", }, {"first": "Léane", "last": "Antoine", }, {"first": "Titouan", "last": "Giraud", }, {"first": "Zoé", "last": "Petit", }, {"first": "Mathilde", "last": "Bébert", }, {"first": "Guillaume Le", "last": "goff", }, {"first": "Julia", "last": "Leclerc", }, {"first": "Léa", "last": "Jean", }, {"first": "Lou", "last": "Hubert", }, {"first": "Timothée", "last": "Marchal", }, {"first": "Françoise", "last": "Gilbert", }, {"first": "Léonard", "last": "Lemoine", }, {"first": "Alexandre", "last": "Guerin", }, {"first": "Marie-léontine", "last": "Hamon", }, {"first": "Amélie", "last": "Martin", }, {"first": "Jade", "last": "Marie", }, {"first": "Gabin", "last": "Girard", }, {"first": "Salomé", "last": "Rodriguez", }, {"first": "Loevan", "last": "Noel", }, {"first": "Antonin", "last": "Carpentier", }, {"first": "Gilbert", "last": "Deschamps", }, {"first": "Alexis", "last": "Millet", }, {"first": "Martin", "last": "Andre", }, {"first": "Antonin", "last": "Gauthier", }, {"first": "Capucine", "last": "Hamon", }, {"first": "Lauriane", "last": "Clement", }, {"first": "Amandine", "last": "Deschamps", }, {"first": "Edwige", "last": "Deschamps", }, {"first": "Clémence", "last": "Tessier", }, {"first": "Jérémy", "last": "Andre", }, {"first": "Chaïma", "last": "Fleury", }, {"first": "Ambre", "last": "Jacquet", }, {"first": "Maïwenn", "last": "Lecomte", }, {"first": "Pierre", "last": "Rousseau", }, {"first": "Clémence", "last": "Leveque", }, {"first": "Jérémy", "last": "Laporte", }, {"first": "Félix", "last": "Tessier", }, {"first": "Romane", "last": "Leclercq", }, {"first": "Cloé", "last": "Leclerc", }, {"first": "Mohamed", "last": "Lopez", }, {"first": "Titouan", "last": "Lambert", }, {"first": "Yüna", "last": "Lamy", }, {"first": "Maïwenn", "last": "Poulain", }, {"first": "Éléna", "last": "Leclerc", }, {"first": "Inès", "last": "Lemoine", }, {"first": "Paul", "last": "Fontaine", }, {"first": "Lucie", "last": "Nicolas", }, {"first": "Marie-léontine", "last": "Daniel", }, {"first": "Yohan", "last": "Legrand", }, {"first": "Nolan", "last": "Robert", }, {"first": "Titouan", "last": "Berger", }, {"first": "Mélissa", "last": "Leveque", }, {"first": "Louise", "last": "Rousseau", }, {"first": "Lamia", "last": "Leclerc", }, {"first": "Thibault", "last": "Mercier", }, {"first": "Marie-léontine", "last": "Rey", }, {"first": "Lina", "last": "Carpentier", }, {"first": "Lilou", "last": "Caron", } ];
+let falseNames = [{"first": "Lana", "last": "Julien", }, {"first": "Alexandre", "last": "Brunet", }, {"first": "Elsa", "last": "Poirier", }, {"first": "Marc", "last": "Lamy", }, {"first": "Mila", "last": "Simon", }, {"first": "Florentin", "last": "Robert", }, {"first": "Adrian", "last": "Baron", }, {"first": "Anna", "last": "Menard", }, {"first": "Dylan", "last": "Roy", }, {"first": "Robin", "last": "Dupuy", }, {"first": "Colin", "last": "Bailly", }, {"first": "Alexandre", "last": "Antoine", }, {"first": "Pauline", "last": "Gaillard", }, {"first": "Clément", "last": "Andre", }, {"first": "Mateo Le", "last": "gall", }, {"first": "Brandon", "last": "Germain", }, {"first": "Clara", "last": "Hamon", }, {"first": "Florian", "last": "Bertin", }, {"first": "Jasmine", "last": "Baron", }, {"first": "Martin", "last": "Bertrand", }, {"first": "Manon", "last": "Renaud", }, {"first": "Jeanne", "last": "Boyer", }, {"first": "Florentin", "last": "Lamy", }, {"first": "Amandine", "last": "Jacquet", }, {"first": "Yüna", "last": "Monnier", }, {"first": "Arthur", "last": "Gilbert", }, {"first": "Lamia", "last": "Besson", }, {"first": "Ambre", "last": "Roussel", }, {"first": "Antoine", "last": "Antoine", }, {"first": "Julie", "last": "Giraud", }, {"first": "Marion", "last": "Philippe", }, {"first": "Loevan", "last": "Gilbert", }, {"first": "Florian", "last": "Mercier", }, {"first": "Mathilde", "last": "Garcia", }, {"first": "Françoise", "last": "Denis", }, {"first": "Zacharis", "last": "Besson", }, {"first": "Ethan", "last": "Garcia", }, {"first": "Yohan", "last": "Fleury", }, {"first": "Candice", "last": "Denis", }, {"first": "Loevan Le", "last": "roux", }, {"first": "Adam", "last": "Rousseau", }, {"first": "Lamia Le", "last": "gall", }, {"first": "Lilian", "last": "Etienne", }, {"first": "Léane", "last": "Antoine", }, {"first": "Titouan", "last": "Giraud", }, {"first": "Zoé", "last": "Petit", }, {"first": "Mathilde", "last": "Bébert", }, {"first": "Guillaume Le", "last": "goff", }, {"first": "Julia", "last": "Leclerc", }, {"first": "Léa", "last": "Jean", }, {"first": "Lou", "last": "Hubert", }, {"first": "Timothée", "last": "Marchal", }, {"first": "Françoise", "last": "Gilbert", }, {"first": "Léonard", "last": "Lemoine", }, {"first": "Alexandre", "last": "Guerin", }, {"first": "Marie-léontine", "last": "Hamon", }, {"first": "Amélie", "last": "Martin", }, {"first": "Jade", "last": "Marie", }, {"first": "Gabin", "last": "Girard", }, {"first": "Salomé", "last": "Rodriguez", }, {"first": "Loevan", "last": "Noel", }, {"first": "Antonin", "last": "Carpentier", }, {"first": "Gilbert", "last": "Deschamps", }, {"first": "Alexis", "last": "Millet", }, {"first": "Martin", "last": "Andre", }, {"first": "Antonin", "last": "Gauthier", }, {"first": "Capucine", "last": "Hamon", }, {"first": "Lauriane", "last": "Clement", }, {"first": "Amandine", "last": "Deschamps", }, {"first": "Edwige", "last": "Deschamps", }, {"first": "Clémence", "last": "Tessier", }, {"first": "Jérémy", "last": "Andre", }, {"first": "Chaïma", "last": "Fleury", }, {"first": "Ambre", "last": "Jacquet", }, {"first": "Maïwenn", "last": "Lecomte", }, {"first": "Pierre", "last": "Rousseau", }, {"first": "Clémence", "last": "Leveque", }, {"first": "Jérémy", "last": "Laporte", }, {"first": "Félix", "last": "Tessier", }, {"first": "Romane", "last": "Leclercq", }, {"first": "Cloé", "last": "Leclerc", }, {"first": "Mohamed", "last": "Lopez", }, {"first": "Titouan", "last": "Lambert", }, {"first": "Yüna", "last": "Lamy", }, {"first": "Maïwenn", "last": "Poulain", }, {"first": "Éléna", "last": "Leclerc", }, {"first": "Inès", "last": "Lemoine", }, {"first": "Paul", "last": "Fontaine", }, {"first": "Lucie", "last": "Nicolas", }, {"first": "Marie-léontine", "last": "Daniel", }, {"first": "Yohan", "last": "Legrand", }, {"first": "Nolan", "last": "Robert", }, {"first": "Titouan", "last": "Berger", }, {"first": "Mélissa", "last": "Leveque", }, {"first": "Louise", "last": "Rousseau", }, {"first": "Lamia", "last": "Leclerc", }, {"first": "Thibault", "last": "Mercier", }, {"first": "Marie-léontine", "last": "Rey", }, {"first": "Lina", "last": "Carpentier", }, {"first": "Lilou", "last": "Caron", } ];
 
 app.get('/PRUEBAS/USER', (req, res) => {
-  generateUsers(t + b);
+  generateUsers(t + b + 1);
   res.send(200);
 })
 
@@ -123,13 +123,14 @@ function generateUsers(amount) {
         password: "74b87337454200d4d33f80c4663dc5e5",//aaaa
         passwordConfirmation: "74b87337454200d4d33f80c4663dc5e5",
         dateOfBirth: new Date(1998, getRandomInt(0, 11), 29),
-        email: `${falseNames[i].first}${i}@ucol.mx`
+        email: `${falseNames[i].first}-${getRandomInt(0, 100)}@ucol.mx`
       });
 
       user.save().then(function(userSaved) {
         console.log("User ", i);
       }).catch(function(err) {
         console.log(err.message);
+        console.log(`${falseNames[i].first}-${getRandomInt(0, 100)}@ucol.mx`);
       })
   }
 }
@@ -180,7 +181,8 @@ app.get('/PRUEBAS/QUALIFY/:idAnnoun', (req, res) => {
   projectsEvaluator.find({idAnnouncement: req.params.idAnnoun})
     .then(projs => {
       projs.forEach(pro => {
-        pro.grade = getRandomInt(5, 11);
+        pro.grade = getRandomInt(0, 11);
+        // pro.grade = -1;
         pro.save()
           .then(res => {
             console.log("Calificado");
@@ -189,6 +191,14 @@ app.get('/PRUEBAS/QUALIFY/:idAnnoun', (req, res) => {
       })
     })
     .catch(err => {console.log(err.message); res.json({err: err.message});})
+})
+
+app.get('/deleteAllUSer', (req, res) => {
+  User.deleteMany({_id: {$not: {$eq: '5b01d106fc5ae925acfbce0d'}}})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {console.log(err); res.json({err: err.message});})
 })
 //Pruebas de scripts para generar usuario etc
 
--- public/vue/controllers/announcement/projectsPerEvaluator2.js
+++ public/vue/controllers/announcement/projectsPerEvaluator2.js
@@ -4,12 +4,37 @@ const vm = new Vue({
 	el: "#app2",
 	data: {
 		evaluators: [],
-		projectsPerEvaluator: []
+		projectsPerEvaluator: [],
+		isComplete: true, //Todos los proyectos ya fueron evaluados, ya se pueden calcular los promedios y las calificaciones ajustadas
+		isNormalMean: false,	//Ya se calificaron los promedios normales
+		isAdjustedGrades: false,	//Ya se calificaron las califiacaciones ajustadas con el modelo
 	},
 	created() {
 		window.axios.get(`${url}api/evaluator/announcement/asignedProject/${idAnnoun}`)
 			.then(({data}) => {	
 				console.log(data);
+				if(data.allEvaluatorProjects[0].length == 0)
+					this.isComplete = false;
+
+				for (var i = data.allEvaluatorProjects.length - 1; i >= 0; i--) {
+					for (var j = data.allEvaluatorProjects[i].length - 1; j >= 0; j--) {
+						if(data.allEvaluatorProjects[i][j].grade < 0) { //Falta por lo menos una proyecto por evaluar
+							// this.isComplete = false;
+							break;
+						}
+						else if(data.allEvaluatorProjects[i][j].idProject.mean > -1) {
+							this.isNormalMean = true;
+							if(data.allEvaluatorProjects[i][j].idProject.adjustedGrade > -1) {
+								this.isAdjustedGrade = true;
+								window.location = `${url}announcement/results/${idAnnoun}`; //Ya están listos los resultados
+							}
+							break;
+						}
+					}
+					if(!this.isComplete || this.isNormalMean)
+						break;		
+				}
+
 				this.evaluators = data.evaluators;
 				this.projectsPerEvaluator = data.allEvaluatorProjects;
 			})
@@ -26,6 +51,22 @@ const vm = new Vue({
 					console.log(data);
 					alert('Recordatorio enviado');
 					this.get(this.evaluators[index]._id).style.visibility = 'hidden';
+				})
+				.catch(err => {console.log(err);})
+		},
+		calculateAdjustedGrades: function() {
+			window.axios.get(`${url}api/project/adjustedGrades/${idAnnoun}`)
+				.then(({data}) => {
+					console.log(data);
+					window.location = `${url}app/announcement/projectsPerEvaluator/${idAnnoun}`;
+				})
+				.catch(err => {console.log(err);})
+		},
+		calculateNormalMeans: function() {
+			window.axios.get(`${url}api/project/calculateNormalMean/${idAnnoun}`)
+				.then(({data}) => {
+					console.log(data);
+					window.location = `${url}app/announcement/projectsPerEvaluator/${idAnnoun}`;
 				})
 				.catch(err => {console.log(err);})
 		}
